1dd30fb05f52c9f57a46dcd5826441144fb01c47
Andrew Bates <abates@omeganetserv.com>
2015-09-03 09:16:12 -0400

    Added license to each source file

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,32 +1,48 @@
+
+/**
+ * Copyright 2015 Andrew Bates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 import java.io.Console;
 import java.util.StringJoiner;
 
 public class Puzzle {
-   public static void main(String []argv) {
-      Console console = System.console();
-      int numStudents = 0;
-      while (numStudents <= 0) {
-         console.printf("Number of students and lockers: ");
-         String v = console.readLine();
-         try {
-            numStudents = Integer.parseInt(v);
-            if (numStudents <= 0) {
-               console.printf("The number of students and lockers must be greater than zero\n");
+    public static void main(String[] argv) {
+        Console console = System.console();
+        int numStudents = 0;
+        while (numStudents <= 0) {
+            console.printf("Number of students and lockers: ");
+            String v = console.readLine();
+            try {
+                numStudents = Integer.parseInt(v);
+                if (numStudents <= 0) {
+                    console.printf("The number of students and lockers must be greater than zero\n");
+                }
+            } catch (NumberFormatException ex) {
+                console.printf("%s is not a number\n", v);
             }
-         } 
-         catch (NumberFormatException ex) {
-            console.printf("%s is not a number\n", v);
-         }
-      }
-      
-      Hallway hallway = new Hallway(numStudents);
-      StudentBody students = new StudentBody(numStudents);
-      students.toggleLockers(hallway);
-      
-      StringJoiner joiner = new StringJoiner(", ");
-      for (int lockerId : hallway.getOpenIds()) {
-         joiner.add(""+lockerId);
-      }
-      console.printf("The following lockers are open: %s\n", joiner.toString());
-   }
+        }
+
+        Hallway hallway = new Hallway(numStudents);
+        StudentBody students = new StudentBody(numStudents);
+        students.toggleLockers(hallway);
+
+        StringJoiner joiner = new StringJoiner(", ");
+        for (int lockerId : hallway.getOpenIds()) {
+            joiner.add("" + lockerId);
+        }
+        console.printf("The following lockers are open: %s\n", joiner.toString());
+    }
 }
--- src/StudentBody.java
+++ src/StudentBody.java
@@ -1,20 +1,37 @@
+
+/**
+ * Copyright 2015 Andrew Bates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 public class StudentBody {
-   private Student[] students;
-   
-   public StudentBody(int numStudents) {
-      students = new Student[numStudents];
-      for (int studentId = 1; studentId <= numStudents; studentId++) {
-         // array indices start at zero, but we don't
-         // want a student ID of zero.  that's why the
-         // loop index starts at one and then we subtract
-         // when using the studentId as an array index :)
-         students[studentId-1] = new Student(studentId);
-      }
-   }
-   
-   public void toggleLockers(Hallway hallway) {
-      for (Student student : students) {
-         student.toggleLockers(hallway);
-      }
-   }
+    private Student[] students;
+
+    public StudentBody(int numStudents) {
+        students = new Student[numStudents];
+        for (int studentId = 1; studentId <= numStudents; studentId++) {
+            // array indices start at zero, but we don't
+            // want a student ID of zero. that's why the
+            // loop index starts at one and then we subtract
+            // when using the studentId as an array index :)
+            students[studentId - 1] = new Student(studentId);
+        }
+    }
+
+    public void toggleLockers(Hallway hallway) {
+        for (Student student : students) {
+            student.toggleLockers(hallway);
+        }
+    }
 }
